b7f066401187f6739369633345874e7d439ec04a
Russ Miles <russ@russmiles.com>
2025-06-02 15:24:31 +0100

    First version of GH template for a Embabel Java Agent Application

---
+++ src/main/java/com/russmiles/EmbabelAgentApplication.java
@@ -0,0 +1,13 @@
+package com.russmiles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmbabelAgentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmbabelAgentApplication.class, args);
+	}
+
+}
